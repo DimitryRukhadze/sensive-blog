97103e33f07105ef897cc3e00429e6096963236f
DimitryRukhadze <lokiinflames@gmail.com>
2022-07-10 19:20:12 +0300

    move queries into QuerySet methods.

--- blog/views.py
+++ blog/views.py
@@ -48,21 +48,9 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = Post.objects.annotate(
-        Count('likes', distinct=True)
-    ).prefetch_related('author').order_by('-likes__count')
-
-    most_popular_posts_ids = [post.id for post in most_popular_posts]
-
-    posts_with_comments = Post.objects.filter(
-        id__in=most_popular_posts_ids
-    ).annotate(comments_count=Count('comments'))
-
-    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
-    count_for_id = dict(ids_and_comments)
-
-    for post in most_popular_posts:
-        post.comments_count = count_for_id[post.id]
+    most_popular_posts = Post.objects.popular()\
+        .prefetch_related('author')[:5]\
+        .fetch_with_comments_count()
 
     most_fresh_posts = Post.objects.annotate(
         comments_count=Count('comments')
@@ -73,7 +61,7 @@ def index(request):
     context = {
         'most_popular_posts': [
             serialize_post_optimized(post)
-            for post in most_popular_posts[:5]
+            for post in most_popular_posts
         ],
         'page_posts': [
             serialize_post_optimized(post)
@@ -116,7 +104,9 @@ def post_detail(request, slug):
 
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.popular() \
+        .prefetch_related('author')[:5] \
+        .fetch_with_comments_count()
 
     context = {
         'post': serialized_post,
@@ -133,7 +123,9 @@ def tag_filter(request, tag_title):
 
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.popular() \
+        .prefetch_related('author')[:5] \
+        .fetch_with_comments_count()
 
     related_posts = tag.posts.annotate(comments_count=Count('comments'))[:20]
 
